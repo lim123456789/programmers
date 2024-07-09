5125dae927619dae6934de988c3a7119a6915dda
lim123456789 <128690117+lim123456789@users.noreply.github.com>
2024-07-09 12:15:28 +0900

    [level 2] Title: 주식가격, Time: 14.67 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    n = len(prices)
+    answer = [0] * n
+    stack = []
+
+    for i in range(n):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    while stack:
+        j = stack.pop()
+        answer[j] = n - 1 - j
+
+    return answer
