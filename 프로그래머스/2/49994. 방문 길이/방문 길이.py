d4f0d541e95d003261fc51d009ad2c49b2cd7884
lim123456789 <128690117+lim123456789@users.noreply.github.com>
2024-07-12 12:19:41 +0900

    [level 2] Title: 방문 길이, Time: 0.33 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -6,11 +6,14 @@ def solution(dirs):
     move = {'U':(0, 1),'D':(0, -1),'R':(1, 0),'L':(-1, 0)}
     
     for i in range(len(dirs)):
-        (dy,dx) = move[dirs[i]]
+        (dx,dy) = move[dirs[i]]
+        
         if not (0 <= x + dx and x + dx <= 10 and 0 <= y + dy and y + dy <= 10):
             continue
+        
         Map.append((x, y, x + dx, y + dy))
         Map.append((x + dx, y + dy, x, y))
+        
         x = x + dx
         y = y + dy
         
