e6228edc35632d1742e3343899cf4866a1a56737
lim123456789 <128690117+lim123456789@users.noreply.github.com>
2024-08-19 12:19:54 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,24 @@
+def check_distance(place):
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    for r in range(5):
+        for c in range(5):
+            if place[r][c] == 'P':
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < 5 and 0 <= nc < 5 and place[nr][nc] == 'P':
+                        return 0
+                    if 0 <= nr < 5 and 0 <= nc < 5 and place[nr][nc] == 'O':
+                        for dr2, dc2 in directions:
+                            nr2, nc2 = nr + dr2, nc + dc2
+                            if nr2 == r and nc2 == c:
+                                continue
+                            if 0 <= nr2 < 5 and 0 <= nc2 < 5 and place[nr2][nc2] == 'P':
+                                return 0
+    return 1
+
+def solution(places):
+    answer = []
+    for place in places:
+        answer.append(check_distance(place))
+    return answer
